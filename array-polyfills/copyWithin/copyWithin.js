55c23e15d7638f0e6a2d14a46846e9e2118d76de
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-09-04 15:36:10 +0530

    feat: array copywithin method polyfill

---
+++ array-polyfills/copyWithin/copyWithin.js
@@ -0,0 +1,24 @@
+Array.prototype.myCopyWithin = function (
+  target = 0,
+  start = 0,
+  end = this.length
+) {
+  if (target < 0) {
+    target = this.length + target;
+  }
+
+  if (start < 0) {
+    start = this.length + start;
+  }
+
+  if (end < 0) {
+    end = this.length + end;
+  }
+
+  for (let i = start; i < end; i++) {
+    this[target] = this[i];
+    target++;
+  }
+
+  return this;
+};
---
+++ array-polyfills/copyWithin/copyWithin.test.js
@@ -0,0 +1,24 @@
+require("./copyWithin");
+
+describe("copyWithin method", () => {
+  test("three params: target, start and end", () => {
+    const arr = ["a", "b", "c", "d", "e"];
+    const result = arr.myCopyWithin(0, 3, 4);
+    const expectedResult = ["d", "b", "c", "d", "e"];
+    expect(result).toEqual(expectedResult);
+  });
+
+  test("two params: target and start", () => {
+    const arr = ["a", "b", "c", "d", "e"];
+    const result = arr.myCopyWithin(1, 3);
+    const expectedResult = ["a", "d", "e", "d", "e"];
+    expect(result).toEqual(expectedResult);
+  });
+
+  test("two params: target and negative start", () => {
+    const arr = ["a", "b", "c", "d", "e"];
+    const result = arr.myCopyWithin(1, -3);
+    const expectedResult = ["a", "c", "d", "e", "e"];
+    expect(result).toEqual(expectedResult);
+  });
+});
