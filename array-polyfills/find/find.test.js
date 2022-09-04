9767c2f88436ee2c875e02f7a065c8d0e6248119
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-09-04 09:29:46 +0530

    fix: test case

--- array-polyfills/find/find.test.js
+++ array-polyfills/find/find.test.js
@@ -10,7 +10,7 @@ describe("find method", () => {
 
   test("element not found", () => {
     const arr = [10, 20, 31, 44, 55, 67];
-    const result = arr.find((element) => element % 3 === 0);
+    const result = arr.myFind((element) => element % 3 === 0);
     expect(result).toBeUndefined();
   });
 });
