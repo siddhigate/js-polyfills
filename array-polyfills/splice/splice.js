bcff63944b2aac3afa552093727c36e2a3701464
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-09-10 21:30:09 +0530

    refactor: splice() array method polyfill

--- array-polyfills/splice/splice.js
+++ array-polyfills/splice/splice.js
@@ -1,114 +1,62 @@
-require("../slice/slice");
-
 Array.prototype.mySplice = function () {
-  let result = [];
-  let length = this.length;
+  let returnResult = [];
+  let start, deleteCount;
+  let items = [];
 
   if (arguments.length === 0) {
-    return result;
+    return returnResult;
   }
 
-  function getStart(start) {
-    let result = 0;
-    if (!isNaN(parseInt(start))) {
-      result = start < 0 ? this.length + parseInt(start) : parseInt(start);
-    }
-
-    return result;
+  // start
+  start = arguments[0];
+  if (start >= this.length) {
+    return returnResult;
+  }
+  if (start < 0) {
+    start = this.length + start;
   }
 
-  function getDeleteCount(start, deleteCount) {
-    let result = 0;
-    if (!isNaN(parseInt(deleteCount))) {
-      result = parseInt(deleteCount);
-
-      if (result > this.length - start) {
-        result = this.length - start;
-      }
-    }
-  }
-
-  function moveLeft(step, start, arr) {
-    for (let i = start; i < arr.length; i++) {
-      arr[i] = arr[i + step];
-    }
-  }
-
-  function moveRight(itemsCount, start, arr) {
-    for (let i = arr.length - 1; i >= start; i--) {
-      arr[i + itemsCount] = arr[i];
-    }
-  }
-
-  // if only start given -> delete els from start to end
+  // deleteCount
   if (arguments.length === 1) {
-    let start = getStart(arguments[0]);
+    deleteCount = this.length - start;
+  }
+  if (arguments.length >= 2) {
+    deleteCount = arguments[1];
 
-    for (let i = this.length - 1; i >= start; i--) {
-      result.unshift(this.pop());
+    if (deleteCount > this.length - start) {
+      deleteCount = this.length - start;
     }
-    return result;
   }
 
-  // if start & delete count given
-  if (arguments.length === 2) {
-    const start = getStart(arguments[0]);
-    const deleteCount = getStart(arguments[1]);
-
-    // items to be deleted
-    result = this.mySlice(start, deleteCount + 1);
-
-    // shift and delete the elements
-    for (let i = start; i < this.length; i++) {
-      this[i] = this[i + deleteCount];
-    }
-
-    for (let i = 0; i < deleteCount; i++) {
-      this.pop();
-    }
-
-    return result;
-  }
-
+  // items
   if (arguments.length > 2) {
-    const start = getStart(arguments[0]);
-    const deleteCount = getStart(arguments[1]);
-
-    if (deleteCount <= 0) {
-      const itemsCount = arguments.length - 2;
-
-      // shift elements forward and add new items
-      moveRight(itemsCount, start, this);
-
-      for (let i = 0; i < itemsCount; i++) {
-        this[i + start] = arguments[2 + i];
-      }
-
-      return result;
-    } else {
-      // remove previous and add new
-
-      // delete prev els
-      // items to be deleted
-      result = this.mySlice(start, deleteCount + 1);
-
-      // shift and delete the elements
-      moveLeft(deleteCount, start, this);
-      for (let i = 0; i < deleteCount; i++) {
-        this.pop();
-      }
-
-      // add new els
-      const itemsCount = arguments.length - 2;
-
-      // shift elements forward and add new items
-      moveRight(itemsCount, start, this);
-
-      for (let i = 0; i < itemsCount; i++) {
-        this[i + start] = arguments[2 + i];
-      }
+    for (let i = 2; i < arguments.length; i++) {
+      items.push(arguments[i]);
     }
   }
 
-  return result;
+  // delete elements if delete count > 0
+  if (deleteCount > 0) {
+    for (let i = 0; i < deleteCount; i++) {
+      returnResult.push(this[start + i]);
+    }
+
+    for (let i = start, j = 0; i <= this.length - deleteCount; i++, j++) {
+      this[start + j] = this[start + j + deleteCount];
+    }
+
+    this.length = this.length - deleteCount;
+  }
+
+  // add elements if items are provided
+  if (items.length > 0) {
+    for (let i = this.length - 1; i >= start; i--) {
+      this[i + items.length] = this[i];
+    }
+
+    for (let i = 0; i < items.length; i++) {
+      this[start + i] = items[i];
+    }
+  }
+  return returnResult;
 };
